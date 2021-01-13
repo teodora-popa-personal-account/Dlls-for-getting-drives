c9e06404dcadba3d645ec8995c2af148d775050b
Teodora Popa <teodorap09@yahoo.com>
2021-01-13 03:02:16 +0200

    Failed with the labels.

--- Labels/Labels/labels.c
+++ Labels/Labels/labels.c
@@ -1,13 +1,34 @@
 #include<stdio.h>
 #include<Windows.h>
 
-__declspec(dllexport) char** GetDriveLabel(char** localDrivePath)
+__declspec(dllexport) char* GetDriveLabel(char* localDrivePath)
 {
-	char* volumeNameBuffer = (char*)malloc((MAX_PATH + 2) * sizeof(char));
-	DWORD volumeSerialNumber;
-	DWORD maximumComponentLength;
-	DWORD fileSystemFlags;
-	char fileSystemNameBuffer[MAX_PATH + 1];
-	GetVolumeInformationA(localDrivePath, volumeNameBuffer, MAX_PATH + 1, &volumeSerialNumber, &maximumComponentLength, &fileSystemFlags, fileSystemNameBuffer, MAX_PATH + 1);
-	return volumeNameBuffer;
+	
+	//char* result = (char*)malloc((2*MAX_PATH) * sizeof(char));
+	//char* volumeNameBuffer = (char*)malloc((MAX_PATH+1) * sizeof(char));
+	//strcpy_s(result, MAX_PATH + 1, "");
+	////strcat_s(volumeNameBuffer, MAX_PATH + 1, localDrivePath);
+
+	///*char* test= (char*)malloc((5) * sizeof(char));
+	//strcpy_s(test, MAX_PATH+1,"");
+	//strcat_s(test, MAX_PATH+1,"C:\\");
+	//strcpy_s(volumeNameBuffer,MAX_PATH+1, "");*/
+	//DWORD volumeSerialNumber;
+	//DWORD maximumComponentLength;
+	//DWORD fileSystemFlags;
+	//char* fileSystemNameBuffer = (char*)malloc((MAX_PATH + 1) * sizeof(char));
+	//GetVolumeInformationA("C:\\", volumeNameBuffer, MAX_PATH + 1, &volumeSerialNumber, &maximumComponentLength, &fileSystemFlags, fileSystemNameBuffer, MAX_PATH + 1);
+
+	//return result;
+	unsigned long  aa = MAX_PATH+1;
+	char VolumeName[MAX_PATH+1];
+	char FileSystemName[MAX_PATH+1];
+	unsigned long  VolumeSerialNo;
+	unsigned long  MaxComponentLength, FileSystemFlags;
+
+	GetVolumeInformation("C:\\", VolumeName, 520, NULL,
+		NULL, NULL,
+		NULL, NULL);
+
+	return VolumeName;
 }
